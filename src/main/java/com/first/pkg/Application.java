38bc7b597e7fe6354f828d3ec539c10d4b2081da
Vasile <vasile.v.ungureanu@gmail.com>
2022-03-31 09:03:28 +0300

    Connect to H2 database

--- src/main/java/com/first/pkg/Application.java
+++ src/main/java/com/first/pkg/Application.java
@@ -2,7 +2,10 @@ package com.first.pkg;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Properties;
+import org.h2.jdbcx.JdbcConnectionPool;
 
 public class Application {
 
@@ -11,6 +14,14 @@ public class Application {
       Properties p = new Properties();
       p.load(is);
       System.out.println(p);
+    }
+
+    JdbcConnectionPool cp = JdbcConnectionPool.
+        create("jdbc:h2:mem:", "sa", "sa"); //
+    try (Connection conn = cp.getConnection()) { //
+      System.out.println("Is my database connection valid: " + conn.isValid(0));
+    } catch (SQLException e) {
+      e.printStackTrace();
     }
   }
 }
